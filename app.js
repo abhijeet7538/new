0064dc69b8df792efca26e35f4cab90052b06a11
Abhijeet <carnage5773@gmail.com>
2023-08-13 19:50:34 +0530

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 const ejs = require("ejs");
 const _ = require("lodash");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "Welcome, fellow starry-eyed Jedi and Sith aficionados, to a corner of the galaxy where the Force is strong and the laughter is even stronger. Our blog isn't just another boring old podracing circuit—it's a lightspeed journey through the far reaches of Star Wars knowledge, sprinkled with enough humor to make even a protocol droid crack a smile. Imagine Yoda sharing knock-knock jokes or Darth Vader attempting stand-up comedy (his 'I am your father' punchline is killer). So, grab your blaster, hop on your speeder, and prepare to warp into a hyperspace of intergalactic chuckles and Jedi-level insights. Whether you're a Padawan or a seasoned Jedi Master, our blog is your hyperdrive to hilarity and wisdom from a galaxy far, far away. May the laughs be with you!";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -14,29 +15,45 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let posts = [];
+mongoose.connect("mongodb+srv://abhi:abhi123@cluster0.achnpco.mongodb.net/blogDB",)
+
+const postSchema = {
+    title: String,
+    content: String
+}
+
+const Post = mongoose.model('Post', postSchema);
+
 
 app.get("/", function (req, res) {
-    res.render("home", {
-        startingContent: homeStartingContent,
-        posts: posts
-    });
+    async function fetchpost() {
 
-});
-
-app.get("/post/:zoro", function (req, res) {
-    const requested_params = _.lowerCase(req.params.zoro);
-    posts.forEach(function (post) {
-        const stored_title = _.lowerCase(post.title);
-        if (stored_title === requested_params) {
-            res.render("post", {
-                title: post.title,
-                content: post.content
+        const posts = await Post.find({})
+        try {
+            res.render("home", {
+                startingContent: homeStartingContent,
+                posts: posts
             });
+        } catch (err) {
+            console.log("an error occured in app.get");
         }
-    });
-
+    }
+    fetchpost();
 });
+
+// app.get("/post/:zoro", function (req, res) {
+//     const requested_params = _.lowerCase(req.params.zoro);
+//     Post.forEach(function (post) {
+//         const stored_title = _.lowerCase(post.title);
+//         if (stored_title === requested_params) {
+//             res.render("post", {
+//                 title: post.title,
+//                 content: post.content
+//             });
+//         }
+//     });
+
+// });
 
 app.get("/about", function (req, res) {
     res.render("about", { about_c: aboutContent });
@@ -51,17 +68,35 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-    const post = {
+    const post = new Post({
         title: req.body.postTitle,
         content: req.body.postBody
-    };
-    posts.push(post);
-
-    res.redirect("/");
+    });
+    async function savepost() {
+        try {
+            post.save();
+            res.redirect("/");
+        }
+        catch (err) {
+            console.log("eroor ocuured in saving");
+        }
+    }
+    savepost();
 });
 
-app.get("/post", function (req, res) {
-    res.render("post");
+app.get("/post/:postId", function (req, res) {
+    const requestedId = req.params.postId;
+    Post.findOne({ _id: requestedId })
+        .then((post) => {
+            res.render("post", {
+                title: post.title,
+                content: post.content
+            });
+        })
+        .catch((err) => {
+            console.log("error ocured in finding");
+        })
+
 });
 
 app.listen(3000, function () {
